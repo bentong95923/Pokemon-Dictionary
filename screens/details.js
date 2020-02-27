7dd805978cb366b55c6944faba4c46136eefdf82
bentong95923 <bentong95923@gmail.com>
2020-02-27 21:20:33 +0800

    added react navigation

---
+++ screens/details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default Details = () => {
+    return (
+        <View>
+            This is Details
+        </View>
+    )
+}
