6618ef41782f21a4656d355cc143c40994f6dbe8
bentong95923 <bentong95923@gmail.com>
2020-02-28 13:52:06 +0800

    successfully read information from response data

--- screens/home.js
+++ screens/home.js
@@ -1,71 +1,92 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import { graphql, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { FlatList } from 'react-native-gesture-handler';
 
-const Get_POKEMON_INFO = gql`
-query {
-    pokemon(name: "Pikachu") {
-    name
-    id
-    image
-    weight {
-      minimum
-      maximum
-    }
-		attacks {
-      fast {
-        name
-        type
-        damage
-      }
-      special {
-        name
-        type
-        damage
-      }
-    }
-    evolutions {
-      name
-      id
-    }
-  }
-}
-`
-
 export default function Home({ navigation }) {
 
-    const [pokemon, setPokemon] = useState("");
-
-    return (
-        <View style={styles.container}>
+    const [pokemonSearchName, setPokemonSearchName] = useState("");
+    const [pokemon, setPokemon] = useState([]);
+    fetchPokemon = (pokemon) => {
+        const Get_POKEMON_INFO = gql`
+        {
+            pokemon(name: "Pikachu") {
+                name
+                id
+                image
+                weight {
+                minimum
+                maximum
+                }
+                attacks {
+                    fast {
+                        name
+                        type
+                        damage
+                    }
+                    special {
+                        name
+                        type
+                        damage
+                    }
+                }
+                evolutions {
+                    name
+                    id
+                }
+            }
+        }          
+        `
+        return (
             <Query query={Get_POKEMON_INFO}>
                 {(response, error) => {
                     if (error) {
-                        console.log('Response Error---------', error);
+                        // console.log('Response Error---------', error);
                         return (<Text>{error}</Text>);
+                    } else if (response.data) {
+                        // console.log('response-data----------', response);
+                        if (response.data.pokemon !== null) {
+                            // Return the FlatList if there is not an error
+                            setPokemon(response.data.pokemon);
+                            return (<Text>Success!</Text>);
+                        }
                     }
-                    if (response) {
-                        console.log('response-data----------', response);
-                        // Return the FlatList if there is not an error
-                        return (<Text>Success!</Text>);
+                    if (response.data) {
+                        // console.log(response.data)
+                    } else {
+                        // console.log(response);
                     }
+                    return (<Text>Failed!</Text>);
                 }}
             </Query>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            {fetchPokemon(pokemonSearchName)}
             <Text>Enter Pokemon:</Text>
             <TextInput
                 style={styles.input}
-                onChangeText={(val) => setPokemon(val)}
+                onChangeText={(val) => setPokemonSearchName(val.trim())}
                 placeholder="e.g. Pikachu"
             />
-            <Text>{pokemon}</Text>
-            <Button title='Go' />
+            <Text>{(pokemon.id) && "Number: " + pokemon.id}</Text>
+            <Text>{(pokemon.name) && "Name: " + pokemon.name}</Text>
+            <View>
+                {pokemon.image &&
+                    <Image
+                        style={{ width: "auto", height: 300 }}
+                        source={{ uri: pokemon.image }}
+                    />}
+                {console.log(pokemon.image)}
+            </View>
+            <Button title='More details'/>
         </View>
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
